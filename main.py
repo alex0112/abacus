0599c660cc0e1cc3fef165689f634c0ce2fdd9f7
alex <alexl@advanstaff.com>
2024-05-20 19:17:42 -0600

    Initial code definitions for issue #1

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+########################################################################
+# Main Module for "Abacus". A UVSim simulator and BasicML interpreter. #
+#                                                                      #
+# Authors:                                                             #
+# Kainny Godinez                                                       #
+# Jackson Jacobson                                                     #
+# Alex Larsen                                                          #
+# Scott Mottola                                                        #
+# Jordan Paxman                                                        #
+########################################################################
+
+def main():
+    """
+    Main function. Starts the simulator and executes a program.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
