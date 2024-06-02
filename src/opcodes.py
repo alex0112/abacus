08f28f86c835b7e1f0f380367e552d04595763ae
jordanpaxman <jordandpax@gmail.com>
2024-06-01 19:00:32 -0600

    Methods in class are operational

--- src/opcodes.py
+++ src/opcodes.py
@@ -1,36 +1,39 @@
-class InvalidOpcodeError(Exception):
-    def __init__(self, raw):
-        self.message = "You done messed up"
-
 class Opcode:
-    """
-    This class is essentially a type to ensure that a piece of data when created is a valid operation code, or at least conforms to the format of a signed four digit base ten number
-    """
+    '''This class is essentially a type to ensure that a piece of data 
+    when created is a valid operation code, 
+    or at least conforms to the format of a signed four digit base ten number'''
 
     def __init__(self, raw):
         self.raw = raw
+        self.sign = ""
 
         if raw[0] != '-' and raw[0] != '+':
             raise ValueError("Must have a + or - at the beginning of opcode")
-        elif len(raw[1:]) != 4:
-            raise
-        if raw[0] == '-':
-            self.sign = '-'
-        elif raw[0] == '+':
-            self.sign = '+'
-        elif len(raw) == 4: ## i.e. if the opcode is unsigned, make it positive by default
-            self.sign = '+'
+        if len(raw[1:]) != 4:
+            raise ValueError("Opcode must only have 4 integers")
+        if len(raw) == 4: ## i.e. if the opcode is unsigned, make it positive by default
+            self.sign = "+"
 
         ## TODO: We could implement other convenience attributes here like a human friendly representation of the opcode
 
-    def operation(self):
+    def get_operation(self):
         #returns operation of the first 2 digits of raw opcode (10 returns READ)
-        pass
-
-    def sign(self):
+        return self.raw[1:3]
+         
+    def get_sign(self):
         #returns sign at the beginning of opcode(+,-)
-        pass
+        return self.raw[0]
 
-    def operand(self):
+    def get_operand(self):
         #returns operand or final 2 digits in raw opcode
-        pass
+        return self.raw[3:]
+    
+    def __str__(self):
+        return self.raw
+    
+opcode = Opcode("+1042")
+
+
+
+
+    
