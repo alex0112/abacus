399e5565a920c55c55623569ba11456fcb688626
jordanpaxman <jordandpax@gmail.com>
2024-06-01 18:25:46 -0600

    Created methods for returning operation, sign, and operand of an opcode object

--- src/opcodes.py
+++ src/opcodes.py
@@ -8,14 +8,12 @@ class Opcode:
     """
 
     def __init__(self, raw):
+        self.raw = raw
 
-        raw = trim
-
-        if raw[0] !== '-' || raw[0] !== '+':
-            raise InvalidOpcodeError(raw) ## Or something
-        elif len(raw[1:]) !== 4:
-            pass ## You get the idea. TODO: write validation logic here
-
+        if raw[0] != '-' and raw[0] != '+':
+            raise ValueError("Must have a + or - at the beginning of opcode")
+        elif len(raw[1:]) != 4:
+            raise
         if raw[0] == '-':
             self.sign = '-'
         elif raw[0] == '+':
@@ -24,3 +22,15 @@ class Opcode:
             self.sign = '+'
 
         ## TODO: We could implement other convenience attributes here like a human friendly representation of the opcode
+
+    def operation(self):
+        #returns operation of the first 2 digits of raw opcode (10 returns READ)
+        pass
+
+    def sign(self):
+        #returns sign at the beginning of opcode(+,-)
+        pass
+
+    def operand(self):
+        #returns operand or final 2 digits in raw opcode
+        pass
