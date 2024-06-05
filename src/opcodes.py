b1a75abd6077143fefb75d29243fb0bd61b2f91f
jordanpaxman <jordandpax@gmail.com>
2024-06-05 00:02:40 -0600

    made private variables

--- src/opcodes.py
+++ src/opcodes.py
@@ -4,16 +4,14 @@ class Opcode:
     or at least conforms to the format of a signed four digit base ten number'''
 
     def __init__(self, raw):
-        self.raw = raw
-        self.sign = ""
-        self.op_list = {10:"READ", 11:"WRITE", 20:"LOAD", 21:"STORE", 30:"ADD", 31:"SUBTRACT", 32:"DIVIDE", 33:"MULTIPLY", 40:"BRANCH", 41:"BRANCHNEG", 42:"BRANCHZERO", 43:"HALT"}
+        self.__raw = raw
+        self.__sign = ""
+        self.__op_list = {10:"READ", 11:"WRITE", 20:"LOAD", 21:"STORE", 30:"ADD", 31:"SUBTRACT", 32:"DIVIDE", 33:"MULTIPLY", 40:"BRANCH", 41:"BRANCHNEG", 42:"BRANCHZERO", 43:"HALT"}
 
-        if raw[0] != '-' and raw[0] != '+':
-            raise ValueError("Must have a + or - at the beginning of opcode")
         if len(raw[1:]) != 4:
             raise ValueError("Opcode must only have 4 integers")
         if len(raw) == 4: ## i.e. if the opcode is unsigned, make it positive by default
-            self.sign = "+"
+            self.__sign = "+"
 
         ## TODO: We could implement other convenience attributes here like a human friendly representation of the opcode
 
@@ -21,12 +19,12 @@ class Opcode:
     def name(self):
         #returns operation of the first 2 digits of raw opcode (10 returns READ)
         operation = int(self.raw[1:3])
-        return self.op_list.get(operation, "NOOP")
+        return self.__op_list.get(operation, "NOOP")
 
     @property
     def sign(self):
         #returns sign at the beginning of opcode(+,-)
-        return self.raw[0]
+        return self.__sign
     
     @property
     def operand(self):
@@ -37,7 +35,7 @@ class Opcode:
         return self.raw
     
 opcode = Opcode("+1042")
-opcode.get_operation()
+# opcode.operation()
 
 
 
