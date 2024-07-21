bb1559f55727f299591746ba3fa0b81e39cf3190
Kainny Godinez <kainny2000@gmail.com>
2024-07-21 17:44:35 -0600

    convert function now is able to raise error or fill shallow lines

--- uvsim.py
+++ uvsim.py
@@ -83,22 +83,47 @@ class UVSim:
         """
         op_list = [10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43]
         new_list = []
-        current = ""
-        with open(filename, 'r') as file:
-            #separate each line to be analized
-            for line in file:
-                if line[0] == "+" or line[0] == "-":
-                    current = line[1:]
-                if int(current[:2]) in op_list:
-                    current = f"{line[0]}0{current[0:2]}0{current[2:]}" #add a 0 to the opcode and 0 to the address
-                else:
-                    current = f"{line[0]}00{current}" #add 00 to the address if is just valie
-                new_list.append(current)
-        #save in file
-        with open(filename, 'w') as file:
-            for line in new_list:
-                file.write(line)
-            print('Program converted successfully')
+        file_content = []
+        if filename == "":
+            print("Filename not provided")
+            return
+        try:
+            with open(filename, 'r') as file:
+                file_content = file.readlines()
+                file = file_content[:]
+                for line in file:
+                    sign_flag = False
+                    current = line.strip()
+                    #check for line structure
+                    if current[0] == "+" or current[0] == "-":
+                        sign_flag = True
+                        current = current[1:]
+                    if len(current) <= 3:
+                        for i in range(4-len(current)):
+                            current = "0" + current
+                        print("Some values where prepended with 0s")
+                    elif len(current) > 4:
+                        raise Exception("File contains invalid opcodes")
+                    if int(current[:2]) in op_list and sign_flag:
+                        current = f"{line[0]}0{current[0:2]}0{current[2:]}\n" #add a 0 to the opcode and 0 to the address
+                    elif int(current[:2]) in op_list and not sign_flag:
+                        current = f"+0{current[0:2]}0{current[2:]}\n"
+                    elif sign_flag:
+                        current = f"{line[0]}00{current}\n" #add 00 to the address if is just value
+                    else:
+                        current = f"+00{current}\n"
+                    new_list.append(current)
+            new_list[-1] = new_list[-1].strip()
+            #save in file
+            with open(filename, 'w') as file:
+                for line in new_list:
+                    file.write(line)
+                print('Program converted successfully')
+        except Exception as e:
+            with open(filename, "w") as file:
+                for line in file_content:
+                    file.write(line)
+                print(f'Error converting the program: {e}')
                     
 
         
